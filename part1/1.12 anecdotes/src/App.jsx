b118478114d6941e2e3932eb3560377e605b697b
RobynIsBlue <tyglivermont@gmail.com>
2025-08-20 09:40:59 -0500

    final anecdotes

--- part1/1.12 anecdotes/src/App.jsx
+++ part1/1.12 anecdotes/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
 
+const MostVoted = ({ mostVoted, anecdotes, votes }) => {
+  console.log(mostVoted);
+  if (votes[mostVoted] === 0) {
+    return <p>No votes have been cast!</p>;
+  }
+  return <p>{anecdotes[mostVoted]}</p>;
+};
+
 const App = () => {
   const anecdotes = [
     "If it hurts, do it more often.",
@@ -11,20 +19,25 @@ const App = () => {
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
     "The only way to go fast, is to go well.",
   ];
-  const votesStart = new Uint8Array(anecdotes.length);
 
-  const [votes, setVotes] = useState(votesStart);
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length));
 
   const [selected, setSelected] = useState(0);
+
+  const [mostVoted, setMostVoted] = useState(0);
 
   const voteInc = () => {
     const copy = { ...votes };
     copy[selected]++;
+    if (copy[selected] > copy[mostVoted]) {
+      setMostVoted(selected);
+    }
     setVotes(copy);
   };
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>Has {votes[selected]} votes</p>
       <button onClick={voteInc}>Vote</button>
@@ -35,6 +48,8 @@ const App = () => {
       >
         Next anecdote?
       </button>
+      <h1>Anecdote with the most votes ({votes[mostVoted]} votes)</h1>
+      <MostVoted anecdotes={anecdotes} mostVoted={mostVoted} votes={votes} />
     </div>
   );
 };
