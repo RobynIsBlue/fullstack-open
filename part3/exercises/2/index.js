40fa9f5c604cd4292a59f7cf9910680841d1ebf5
RobynIsBlue <tyglivermont@gmail.com>
2025-10-20 09:49:48 -0500

    asd

---
+++ part3/exercises/2/index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(express.json());
+morgan.token("borb", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :borb")
+);
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const date = Date().toLocaleString();
+  response.send(
+    `<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>
+    </div>`
+  );
+});
+//a
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (!person) {
+    return response.status(404).end();
+  }
+
+  return response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Missing name and/or number",
+    });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      erorr: "Name already exists",
+    });
+  }
+
+  const ID = Math.floor(Math.random() * 999999);
+  const person = {
+    id: String(ID),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`running at http://localhost:${PORT}`);
+});
